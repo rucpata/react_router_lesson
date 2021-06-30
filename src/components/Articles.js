cecdbb2a94358267b71f85785110b4da5cae38e2
rucpata <rucinska.patrycja@gmail.com>
2021-06-30 10:51:46 +0200

    query parameters

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,16 +4,17 @@ import { selectArticles, filterArticles } from "../features/articles/articlesSli
 import Search from "./Search";
 
 // Import Link from React Router
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 export default function Articles () {
   const articles = useSelector(selectArticles)
   
   // grab the search value from useLocation()
-  
+  const { search } = useLocation();
   // get the queryParams from new URLSearchParams() 
-  
-  const title = '' // <-- fix me!
+  const queryParams = new URLSearchParams(search);
+
+  const title = queryParams.get('title') // <-- fix me!
   
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
